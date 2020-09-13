840718f4ca432336ef8808f33b063f65aeea33da
k3plr <bensel1234@yahoo.com>
2020-09-13 00:39:09 -0400

    Adding readme file

--- bsp.d
+++
@@ -1,9 +0,0 @@
-./bsp.o: bsp.c bsp.h C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h \
-  TM4C123GH6PM_Q.h \
-  C:\Users\benji\AppData\Local\Arm\Packs\ARM\CMSIS\5.7.0\CMSIS\Core\Include\core_cm4.h \
-  C:\Users\benji\AppData\Local\Arm\Packs\ARM\CMSIS\5.7.0\CMSIS\Core\Include\cmsis_version.h \
-  C:\Users\benji\AppData\Local\Arm\Packs\ARM\CMSIS\5.7.0\CMSIS\Core\Include\cmsis_compiler.h \
-  C:\Users\benji\AppData\Local\Arm\Packs\ARM\CMSIS\5.7.0\CMSIS\Core\Include\cmsis_armclang.h \
-  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\arm_compat.h \
-  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\arm_acle.h \
-  C:\Users\benji\AppData\Local\Arm\Packs\ARM\CMSIS\5.7.0\CMSIS\Core\Include\mpu_armv7.h
